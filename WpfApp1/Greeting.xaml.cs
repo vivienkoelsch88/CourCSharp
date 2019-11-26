77b19d1460aab9b2dd213bd796d017e8d1a667e9
vivienkoelsch88 <vivien.koelsch@le-campus-numerique.fr>
2019-11-26 09:41:09 +0100

    Affichage sur wpf

--- WpfApp1/Greeting.xaml.cs
+++ WpfApp1/Greeting.xaml.cs
@@ -21,17 +21,32 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindowC : Window
     {
+        public GreetingViewModel viewModel;
+        public ArrayJson Idtest { get; set; }
 
         public MainWindowC()
         {
             InitializeComponent();
-            
+            this.viewModel = new GreetingViewModel();
+            this.DataContext = this.viewModel;
+            this.Idtest = new ArrayJson
+            {
+                id = "JE TEEEEEST",
+               
+                
+            };
+
         }
 
         private void Btn1_Click_1(object sender, RoutedEventArgs e)
         {
+            this.viewModel.remplirArrets(tbSettingTextLat.Text, tbSettingTextLon.Text, tbSettingTextDist.Text);
+
+              /*
             UtilApi api = new UtilApi(tbSettingTextLat.Text, tbSettingTextLon.Text);
             Dictionary<string, List<ArrayJson>> dictionnaireArret = api.getListLigne(int.Parse(tbSettingTextDist.Text));
+
+          
             String Affiche = "";
 
             foreach (KeyValuePair<string, List<ArrayJson>> element in dictionnaireArret)
@@ -50,7 +65,7 @@ namespace WpfApp1
                 }
             }
             listeArretBox.Text = Affiche;
-
+            */
         }
 
         private void TbSettingTextLat_TextChanged(object sender, TextChangedEventArgs e)
---
+++ WpfApp1/GreetingViewModel.cs
@@ -0,0 +1,83 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class GreetingViewModel : INotifyPropertyChanged
+    {
+        public List<String> listAffich = new List<string>();
+        private Dictionary<string, List<ArrayJson>> arrets;
+        public Dictionary<string, List<ArrayJson>> Arrets
+        {
+            get
+            {
+                return arrets;
+            }
+            set
+            {
+                SetProperty(ref arrets, value);
+            }
+        }
+
+        public GreetingViewModel()
+        {
+            Arrets = new Dictionary<string, List<ArrayJson>>();
+            
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        public void remplirArrets(String lat, String lon, String dist)
+        {
+            UtilApi api = new UtilApi(lat, lon);
+            Dictionary<string, List<ArrayJson>> DictionnaireArrets = api.getListLigne(int.Parse(dist));
+
+            List<String> listeAAfficher = new List<String>();
+            foreach (KeyValuePair<string, List<ArrayJson>> ListArrayJson in DictionnaireArrets)
+            {
+                listeAAfficher.Add(ListArrayJson.Key);
+                /*foreach (ArrayJson objectArray in ListArrayJson.Value)
+                {
+                    foreach (String idLigne in objectArray.lines)
+                    {
+                        listeAAfficher.Add(idLigne);
+                    }
+                }*/
+
+            }
+
+            listAffich = listeAAfficher;
+            Arrets = DictionnaireArrets;
+        }
+
+        protected bool SetProperty(ref Dictionary<string, List<ArrayJson>> storage, Dictionary<string, List<ArrayJson>> value, [CallerMemberName] String propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        ///
+        /// Notifies listeners that a property value has changed.
+        ///
+        ///Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers
+        /// that support .
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+    
